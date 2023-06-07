9928754b3fa82d54d0e984f88d1088b624e39007
Anna Borisenko <anik4366@mail.ru>
2023-06-07 08:49:34 +0700

    error massage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-//структура данных теперь - не просто переменная, которая является строкой, а объект
-
 const posts = [];
+
+const TITLE_VALIDATION_LIMIT = 10;
+const TEXT_VALIDATION_LIMIT = 20;
 
 const postTitleInputNode = document.querySelector('.js-post-title-input');
 const postTextInputNode = document.querySelector('.js-post-text-input');
@@ -12,7 +13,7 @@ newPostBtnNode.addEventListener('click', function () {
   //получить данные из поля ввода
   const postFromUser = getPostFromUser();
 
-  //сохранить пост (в данном случае заголовок поста) - присвой значение, которое ввел пользователь посту (в самом верху)
+  //сохранить пост (в данном случае заголовок поста) - присвой значение, которое ввел пользователь
   addPost(postFromUser);
   //этот метод в массив добавит новый пост
   //отобразить пост
@@ -20,31 +21,56 @@ newPostBtnNode.addEventListener('click', function () {
   renderPosts();
 });
 
-postTitleInputNode.addEventListener('input', function (event) {
-  console.log('change', event.target.value);
-});
+postTitleInputNode.addEventListener('input', validation);
+
+postTextInputNode.addEventListener('input', validation);
+
+function validation() {
+  const titlelen = postTitleInputNode.value.length;
+  const textlen = postTextInputNode.value.length;
+
+  if (titlelen > TITLE_VALIDATION_LIMIT) {
+    validationMessage.innerText = `Длина заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов!`;
+    validationMessage.classList.remove('validationMessage_hidden');
+    return;
+  }
+
+  if (textlen > TEXT_VALIDATION_LIMIT) {
+    validationMessage.innerText = `Длина текста не должна превышать ${TEXT_VALIDATION_LIMIT} символов!`;
+    validationMessage.classList.remove('validationMessage_hidden');
+    return;
+  }
+  validationMessage.classList.add('validationMessage_hidden');
+}
 
 function getPostFromUser() {
   //заголовок получаем из одного поля ввода, а текст из другого
   const title = postTitleInputNode.value;
   const text = postTextInputNode.value;
+  const options = { weekday: 'long', hour: 'numeric', minute: 'numeric' };
+  const date = new Date().toLocaleDateString();
+  const time = new Date().toLocaleTimeString([], options);
+
+  if (!title || !text) {
+    return false;
+  }
 
   return {
-    //возвращаем объект, состоящий из тайтла и текста
+    //возвращаем объект, состоящий из тайтла, текста, даты и времени
     title: title,
     text: text,
+    date: date,
+    time: time,
   };
 }
 
-function addPost({ title, text }) {
+function addPost({ title, text, date, time }) {
   // в эту функцию мы передаем объект, состоящий из тайтла и текста - и потом мы из этого объекта в массив постов запушим новый
-  const currentDate = new Date();
-  const dt = `${currentDate.getHours()}:${currentDate.getMinutes()}`;
-
   posts.push({
-    dt,
     title,
     text,
+    date,
+    time,
   });
 }
 
@@ -61,20 +87,12 @@ function renderPosts() {
   posts.forEach((post) => {
     postsHTML += `  
       <div class='post'>
-          <p class='post__date'>${post.dt}</p>
+          <p class='post__date'>${post.date} ${post.time}</p>
           <p class='post__title'>${post.title}</p>
           <p class='post__text'>${post.text}</p>
       </div>
     `;
   });
-
-  // const postHTML = `
-  //   <div class='post'>
-  //       <p class='post__title'>${post.title}</p>
-  //       <p class='post__text'>${post.text}</p>
-  //   </div>
-  //   `;
-  //формирование HTML через шаблонные строки с обратными кавычками
 
   postsNode.innerHTML = postsHTML;
   //вставляем этот HTML в Ленту posts
